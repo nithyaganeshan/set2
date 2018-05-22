bd998e21c0c3a095f7b81de8eb62944428df6c9c
nithyaganeshan <36395607+nithyaganeshan@users.noreply.github.com>
2018-05-22 14:01:24 +0530

    Create nn.c

---
+++ nn.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,a,b,c,i;
+printf("enter the element");
+scanf("%d",&n);
+a=0;
+b=1;
+printf("%d%d",a,b);
+for(i=1;i<=n;i++)
+{
+c=a+b;
+a=b;
+b=c;
+printf("%d",c);
+}
+return 0;
+}

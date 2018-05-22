9565d7843c3b312cedf4831a948f7aebaf6b44c9
nithyaganeshan <36395607+nithyaganeshan@users.noreply.github.com>
2018-05-22 16:30:13 +0530

    Create print.c

---
+++ print.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include<string.h>
+
+int main(void) 
+
+{
+
+	char a[100];
+
+	int n,i,num;
+
+	scanf("%s",a);
+
+	scanf("%d",&num);
+
+	n=strlen(a);
+
+    for(i=0;i<num;i++)
+
+    {
+
+        printf("%c",a[i]);
+
+    }
+
+	return 0;
+
+}

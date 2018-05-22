da461ddab7458e7653706d4b949759e95c56ec0a
nithyaganeshan <36395607+nithyaganeshan@users.noreply.github.com>
2018-05-22 09:31:43 +0530

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int n,a[i];
+printf("enter the number");
+scanf("%d",n);
+for(int i=0;i<=n;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<=n;i++)
+{
+for(int j=i+1;j<=n;j++)
+{
+if(a[i]<=a[j])
+{
+{
+a[i]=a[j];
+}
+printf("%d",a[i]);
+}
+else
+{
+printf("%d",a[i])
+}
+}
+}
+}

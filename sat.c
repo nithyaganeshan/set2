3f18493edeb11a1c32388cba4ae1b76593c4799f
nithyaganeshan <36395607+nithyaganeshan@users.noreply.github.com>
2018-05-23 14:04:57 +0530

    Create sat.c

---
+++ sat.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\nEnter the number");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("\nMultiple by 13");
+}
+else
+{
+printf("\nNot multiple by 13");
+}
+getch();
+}
